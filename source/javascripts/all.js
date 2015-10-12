8243e25230be7d78dba09570c3b7de11ca7e0fa9
jnf <jeremy.flores@gmail.com>
2015-10-12 10:51:26 -0700

    Port static assets from sintra site; begin haml conversion of views.

---
+++ source/javascripts/all.js
@@ -0,0 +1,2 @@
+//= require bootstrap.min.js
+//= require app
---
+++ source/javascripts/app.js
@@ -0,0 +1,16 @@
+// Fix hamburger display for back button
+var spinner = $("#spinner-form");
+
+if (spinner.is(":checked")) {
+  spinner.prop("checked", false);
+}
+
+// Toggle mennu
+$("#menu-toggle").click(function(e) {
+  $("#wrapper").toggleClass("toggled");
+});
+
+// enable the bio tooltips on page ready
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
